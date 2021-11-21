dba855b4eaaa80808fce4f053620d99ad3cc9b4b
Johnny <393116105@qq.com>
2021-11-21 17:08:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,14 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+ZZZ=package/lean/default-settings/files/zzz-default-settings
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate							# 修改默认ip
+sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd										# 替换终端为bash
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile						# 选择argon为默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile				# 选择argon为默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile			# 选择argon为默认主题
+sed -i 's/+uhttpd +uhttpd-mod-ubus //g' feeds/luci/collections/luci/Makefile								# 删除uhttpd
+sed -i 's/OpenWrt /${Author} compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g' $ZZZ					# 增加个性名字 ${Author} 默认为你的github帐号
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ																		# 设置密码为空
+sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/x86/Makefile											# x86机型,默认内核5.4，修改内核为5.10
+git clone https://github.com/DevOpenWRT-Router/luci-app-rebootschedule package/luci-app-rebootschedule		# 拉取计划任务
