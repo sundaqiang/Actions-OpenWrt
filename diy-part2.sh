e997479037e632f4dd4ef1f8291f2f590e04e4bf
Johnny <393116105@qq.com>
2021-11-21 19:03:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,4 +21,3 @@ sed -i 's/+uhttpd +uhttpd-mod-ubus //g' feeds/luci/collections/luci/Makefile				
 sed -i 's/OpenWrt /${Author} compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g' $ZZZ					# 增加个性名字 ${Author} 默认为你的github帐号
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ																		# 设置密码为空
 sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/x86/Makefile											# x86机型,默认内核5.4，修改内核为5.10
-git clone https://github.com/DevOpenWRT-Router/luci-app-rebootschedule package/luci-app-rebootschedule		# 拉取计划任务
